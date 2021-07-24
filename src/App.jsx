9627fddb492c3a3d0668326eba8f1397529d5a1c
mike.m <marachev@kpr-it.ru>
2021-07-24 17:00:37 +0300

    update item withRouter navigation HOC

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import './App.css';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom'
 import Homepage from './pages/Homepage';
 
+import './App.css';
+
+const HatsPage = () => {
+   return(
+      <div>
+         <h1> HATS PAGE </h1>
+      </div>
+   )
+};
+
 function App() {
-  return (
-    <div className="App">
-      <Homepage></Homepage>
-    </div>
-  );
+   return (
+     <div className="App">
+        <Switch>
+           <Route exact path='/' component={Homepage} />
+           <Route path='/shop/hats' component={HatsPage} />
+        </Switch>
+     </div>
+   );
 }
 
 export default App;
--- src/components/directory/directory.jsx
+++ src/components/directory/directory.jsx
@@ -49,13 +49,9 @@ export default class Directory extends React.Component  {
       return (
          <div className='directory-menu'>
             {
-               this.state.sections.map(({ title, imageUrl, id, size }) => (
+               this.state.sections.map(({ id, ...sectionProps }) => (
                   <MenuItem
-                     key={id}
-                     title={title}
-                     imageUrl={imageUrl}
-                     size={size}
-                     subtitle={'SHOP NOW'}
+                     key={id} {...sectionProps}
                   />
                ))}
             }
