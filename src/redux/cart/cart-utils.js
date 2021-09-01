e2864c728df628e266ad95c7888ec527e187cd13
mike.m <marachev@kpr-it.ru>
2021-09-01 22:45:52 +0300

    adding remove add delete items actions to ckeckout-item component

--- src/redux/cart/cart-utils.js
+++ src/redux/cart/cart-utils.js
@@ -1,7 +1,7 @@
 export const addItemToCart = (cartItems, cartItemsToAdd) => {
    const isExists = cartItems.find(
       cartItem => cartItem.id === cartItemsToAdd.id
-   )
+   );
 
    if (isExists) {
       return cartItems.map(cartItem =>
@@ -12,4 +12,21 @@ export const addItemToCart = (cartItems, cartItemsToAdd) => {
    }
 
    return [ ...cartItems, { ...cartItemsToAdd, quantity: 1 }];
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+   const existingItem = cartItems.find(
+      cartItem => cartItem.id === cartItemToRemove.id
+   );
+
+   if (existingItem && existingItem.quantity === 1) {
+      return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+   } else {
+      return cartItems.map(
+         cartItem =>
+            cartItem.id === cartItemToRemove.id
+            ? { ...cartItem, quantity: cartItem.quantity - 1}
+            : cartItem
+      )
+   }
 };
