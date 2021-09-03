1e70747a2c030dab141dcff78a853eb74e3f3d97
mike.m <marachev@kpr-it.ru>
2021-09-03 17:28:50 +0300

    adding redux-persist and modify store in root-store

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,21 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user-reducer';
 import cartReducer from './cart/cart-reducer';
 
-export default combineReducers({
+//whiteList - the only reducers we want to persist (hold)
+//storage - storage(sessionStorage)
+const persistConfig = {
+  key: 'root',
+  storage,
+  whiteList: ['cart']
+};
+
+const rootReducer = combineReducers({
    user: userReducer,
    cart: cartReducer
-})
+});
+
+export default persistReducer(persistConfig, rootReducer);
